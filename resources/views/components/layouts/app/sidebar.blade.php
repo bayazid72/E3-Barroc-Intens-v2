b8524dbd6c6c5e3d0181a600953748958c6061e2
2Ran076 <D290119@edu.curio.nl>
2025-11-18 11:12:52 +0100

    kleine bug fix
    logout knop toonde niet bij een smalle window.

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -110,7 +110,7 @@
 
 
     {{-- PROFILE / LOGOUT --}}
-    <flux:dropdown class="hidden lg:block" position="bottom" align="start">
+    <flux:dropdown class="mt-auto" position="bottom" align="start">
         <flux:profile
             :name="auth()->user()->name"
             :initials="auth()->user()->initials()"
